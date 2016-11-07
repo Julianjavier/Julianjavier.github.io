472d1faf0e03046662f339dab7d4efe830f2c636
Julian Rodriguez <greyfox893@gmail.com>
2016-11-07 17:30:41 -0400

    reworked the nav bar to chnge colors on scroll.

--- UAV-IQ-Marketing/app/js/main.js
+++ UAV-IQ-Marketing/app/js/main.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
     console.log(this);
     $(".nav").toggleClass("show");
   });
+
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 20) {
+        console.log(this);
+          $(".navbar").addClass("changeHeader");
+          $(".navbar .navbar-brand img").attr("src","img/uav-iq-precision-agriculture-logo-horizontal.png");
+      } else {
+          $(".navbar").removeClass("changeHeader");
+          $(".navbar .navbar-brand img").attr("src","img/uav-iq-precision-agriculture-logo-horizontal_white.png");
+      }
+  });
 })
