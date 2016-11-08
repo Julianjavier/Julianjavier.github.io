13763edb2cf36c992a6700486e2e1df5504927cd
Julian Rodriguez <greyfox893@gmail.com>
2016-11-08 19:20:03 -0400

    reverted some changes since they conflicted with original design.

--- UAV-IQ-Marketing/app/js/main.js
+++ UAV-IQ-Marketing/app/js/main.js
@@ -11,9 +11,11 @@ $(document).ready(function(){
       if (scroll >= 20) {
         console.log(this);
           $(".navbar").addClass("changeHeader");
+          $(".droneOperators").addClass("displayNone");
           $(".navbar .navbar-brand img").attr("src","img/uav-iq-precision-agriculture-logo-horizontal.png");
       } else {
           $(".navbar").removeClass("changeHeader");
+          $(".droneOperators").removeClass("displayNone");
           $(".navbar .navbar-brand img").attr("src","img/uav-iq-precision-agriculture-logo-horizontal_white.png");
       }
   });
