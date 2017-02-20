b46a4ae7a51ea442327e4cd015a82d4cf5abea27
Julian Rodriguez <julianjavier@fullsail.com>
2017-02-19 21:44:06 -0400

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,8 @@ var Store_Type_Number = 14;
 var SCV_ID = 32;
 var User_Number = 171;
 
+var dataSource = [{"Product_Number":"174","Store_Type_Number":"14","Market_Number":"1","Chain_Number":"161","SCV_ID":"32","Brand_Number":"40","Subbrand_Number":"0","Category_Number":"1","Description":"Heineken Green 6 pack 12. oz.","Capacity":"12 oz.","Case_Count":"4","Package_Count":"6","UPC":"72890000217","Product_Alias":"H02","Product_Image":"P827.jpg","Created":"2017-01-27 20:54:47","Updated":"2017-01-27 20:54:47","Company_Number":"9","Product_Company_Number":"400141","Product_Goal_ID":"27254","Stock_Goal":"808888","Facings_Goal":"12888","Min_Price":"7000000","Max_Price":"8888890000","Promo_Price_Goal":"89089080","Effective_Date":"2016-08-18"},{"Product_Number":"420","Store_Type_Number":"14","Market_Number":"1","Chain_Number":"39","SCV_ID":"32","Brand_Number":"40","Subbrand_Number":"0","Category_Number":"34","Description":"Heineken 12oz SS","Capacity":"SS","Case_Count":"24","Package_Count":"1","UPC":"72890000316","Product_Alias":"H13","Product_Image":"P420.jpg","Created":"2016-03-01 00:00:00","Updated":"2016-03-01 00:00:00","Company_Number":"9","Product_Company_Number":"500141","Product_Goal_ID":"1138","Stock_Goal":"80","Facings_Goal":"1","Min_Price":"2","Max_Price":"2.75","Promo_Price_Goal":" 2 x $4.00 ","Effective_Date":"2016-03-01"},{"Product_Number":"421","Store_Type_Number":"14","Market_Number":"1","Chain_Number":"39","SCV_ID":"32","Brand_Number":"40","Subbrand_Number":"0","Category_Number":"34","Description":"Heineken 8.5oz SS","Capacity":"SS","Case_Count":"0","Package_Count":"0","UPC":"573000000000","Product_Alias":"H14","Product_Image":"P421.jpg","Created":"2016-03-01 00:00:00","Updated":"2016-03-01 00:00:00","Company_Number":"9","Product_Company_Number":"500165","Product_Goal_ID":"1157","Stock_Goal":"80","Facings_Goal":"1","Min_Price":"1.25","Max_Price":"1.5","Promo_Price_Goal":" 4 x $5.00 ","Effective_Date":"2016-03-01"},{"Product_Number":"901","Store_Type_Number":"14","Market_Number":"1","Chain_Number":"39","SCV_ID":"32","Brand_Number":"40","Subbrand_Number":"0","Category_Number":"34","Description":"Heineken Light  12OZ SS","Capacity":"SS","Case_Count":"0","Package_Count":"0","UPC":"0","Product_Alias":"H20","Product_Image":"P852.jpg","Created":"2016-03-01 00:00:00","Updated":"2016-03-01 00:00:00","Company_Number":"9","Product_Company_Number":"500140","Product_Goal_ID":"5936","Stock_Goal":"80","Facings_Goal":"1","Min_Price":"2","Max_Price":"2.75","Promo_Price_Goal":" 2 x $4.00 ","Effective_Date":"2016-03-01"},{"Product_Number":"903","Store_Type_Number":"14","Market_Number":"1","Chain_Number":"39","SCV_ID":"32","Brand_Number":"40","Subbrand_Number":"0","Category_Number":"34","Description":"Heineken LIGHT 8.5OZ SS","Capacity":"SS","Case_Count":"1","Package_Count":"0","UPC":"0","Product_Alias":"H21","Product_Image":"P853.jpg","Created":"2016-03-01 00:00:00","Updated":"2016-03-01 00:00:00","Company_Number":"9","Product_Company_Number":"500393","Product_Goal_ID":"5969","Stock_Goal":"80","Facings_Goal":"1","Min_Price":"1.25","Max_Price":"1.5","Promo_Price_Goal":" 4 x $5.00 ","Effective_Date":"2016-03-01"}];
+
 $(document).ready(function() {
 	// Table setup
 	// ------------------------------
@@ -29,10 +31,7 @@ $(document).ready(function() {
 	});
 
   var table = $('.table').DataTable( {
-		ajax:{
-			url: 'scripts/php/getProduct.php?SCV_ID='+SCV_ID+'&User_Number='+User_Number+'&Store_Type_Number='+Store_Type_Number,
-			dataSrc:""
-		},
+    data: dataSource,
     columns: [
       {
 				title: "SKU",
