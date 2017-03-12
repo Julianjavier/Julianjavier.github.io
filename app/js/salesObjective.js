4dbf3bd6daf6a300d803d05fecf71bd2b7477e76
Julian Rodriguez <jrodriguez@abartyshealth.com>
2017-03-12 18:54:23 -0400

    testing d3

--- app/js/salesObjective.js
+++ app/js/salesObjective.js
@@ -9,141 +9,6 @@ var basicTable = [
 var totalHits = 0;
 
 $(document).ready(function() {
-	// Animated progress chart
-	// ------------------------------
-
-	// Initialize charts
-	progressCounter('#hours-available-progress', 17, 2, "#9CCC65", 0.93, "icon-coin-dollar text-green-400", 'SALES', '')
-
-	// Chart setup
-	function progressCounter(element, radius, border, color, end, iconClass, textTitle, textAverage){
-
-
-			// Basic setup
-			// ------------------------------
-
-			// Main variables
-			var d3Container = d3.select(element),
-					startPercent = 0,
-					iconSize = radius,
-					endPercent = end,
-					twoPi = Math.PI * 2,
-					formatPercent = d3.format('.0%'),
-					boxSize = radius * 2;
-
-			// Values count
-			var count = Math.abs((endPercent - startPercent) / 0.01);
-
-			// Values step
-			var step = endPercent < startPercent ? -0.01 : 0.01;
-
-
-
-			// Create chart
-			// ------------------------------
-
-			// Add SVG element
-			var container = d3Containerappend('svg');
-
-			// Add SVG group
-			var svg = container
-					.attr('width', boxSize)
-					.attr('height', boxSize)
-					append('g')
-							.attr('transform', 'translate(' + (boxSize / 2) + ',' + (boxSize / 2) + ')');
-
-
-
-			// Construct chart layout
-			// ------------------------------
-
-			// Arc
-			var arc = d3.svg.arc()
-					.startAngle(0)
-					.innerRadius(radius)
-					.outerRadius(radius - border);
-
-
-
-			//
-			// Append chart elements
-			//
-
-			// Paths
-			// ------------------------------
-
-			// Background path
-			svgappend('path')
-					.attr('class', 'd3-progress-background')
-					.attr('d', arc.endAngle(twoPi))
-					.style('fill', '#eee');
-
-			// Foreground path
-			var foreground = svgappend('path')
-					.attr('class', 'd3-progress-foreground')
-					.attr('filter', 'url(#blur)')
-					.style('fill', color)
-					.style('stroke', color);
-
-			// Front path
-			var front = svgappend('path')
-					.attr('class', 'd3-progress-front')
-					.style('fill', color)
-					.style('fill-opacity', 1);
-
-
-
-			// Text
-			// ------------------------------
-
-			// Percentage text value
-			var numberText = d3.select(element)
-					append('h2')
-							.attr('class', 'mt-15 mb-5')
-
-			// Icon
-			d3.select(element)
-					append("i")
-							.attr("class", iconClass + " counter-icon")
-							.attr('style', 'top: ' + ((boxSize - iconSize) / 2) + 'px;' + 'font-size: ' + iconSize + 'px;' + 'margin-left: -'+ ((boxSize - iconSize) / 2) +'px;');
-
-
-			// Title
-			d3.select(element)
-					append('div')
-							.text(textTitle);
-
-			// Subtitle
-			d3.select(element)
-					append('div')
-							.attr('class', 'text-size-small text-muted')
-							.text(textAverage);
-
-
-
-			// Animation
-			// ------------------------------
-
-			// Animate path
-			function updateProgress(progress) {
-					foreground.attr('d', arc.endAngle(twoPi * progress));
-					front.attr('d', arc.endAngle(twoPi * progress));
-					numberText.text(formatPercent(progress));
-			}
-
-			// Animate text
-			var progress = startPercent;
-			(function loops() {
-					updateProgress(progress);
-					if (count > 0) {
-							count--;
-							progress += step;
-							setTimeout(loops, 10);
-					}
-			})();
-	}
-
-
 	function setNewTotalHits(){
 		var salesPlan = parseFloat($("#salesPlan").html()),
 				promoBeer = parseFloat($("#promoBeer").html()),
@@ -295,4 +160,139 @@ $(document).ready(function() {
 			$($.fn.dataTable.tables( true ) ).css('width', '100%');
 			$($.fn.dataTable.tables( true ) ).DataTable().columns.adjust().draw();
 	} );
+
+
+	// Animated progress chart
+	// ------------------------------
+
+	// Initialize charts
+	progressCounter('#hours-available-progress', 17, 2, "#9CCC65", 0.93, "icon-coin-dollar text-green-400", 'SALES', '')
+
+	// Chart setup
+	function progressCounter(element, radius, border, color, end, iconClass, textTitle, textAverage){
+
+
+			// Basic setup
+			// ------------------------------
+
+			// Main variables
+			var d3Container = d3.select(element),
+					startPercent = 0,
+					iconSize = radius,
+					endPercent = end,
+					twoPi = Math.PI * 2,
+					formatPercent = d3.format('.0%'),
+					boxSize = radius * 2;
+
+			// Values count
+			var count = Math.abs((endPercent - startPercent) / 0.01);
+
+			// Values step
+			var step = endPercent < startPercent ? -0.01 : 0.01;
+
+
+
+			// Create chart
+			// ------------------------------
+
+			// Add SVG element
+			var container = d3Containerappend('svg');
+
+			// Add SVG group
+			var svg = container
+					.attr('width', boxSize)
+					.attr('height', boxSize)
+					append('g')
+							.attr('transform', 'translate(' + (boxSize / 2) + ',' + (boxSize / 2) + ')');
+
+
+
+			// Construct chart layout
+			// ------------------------------
+
+			// Arc
+			var arc = d3.svg.arc()
+					.startAngle(0)
+					.innerRadius(radius)
+					.outerRadius(radius - border);
+
+
+
+			//
+			// Append chart elements
+			//
+
+			// Paths
+			// ------------------------------
+
+			// Background path
+			svgappend('path')
+					.attr('class', 'd3-progress-background')
+					.attr('d', arc.endAngle(twoPi))
+					.style('fill', '#eee');
+
+			// Foreground path
+			var foreground = svgappend('path')
+					.attr('class', 'd3-progress-foreground')
+					.attr('filter', 'url(#blur)')
+					.style('fill', color)
+					.style('stroke', color);
+
+			// Front path
+			var front = svgappend('path')
+					.attr('class', 'd3-progress-front')
+					.style('fill', color)
+					.style('fill-opacity', 1);
+
+
+
+			// Text
+			// ------------------------------
+
+			// Percentage text value
+			var numberText = d3.select(element)
+					append('h2')
+							.attr('class', 'mt-15 mb-5')
+
+			// Icon
+			d3.select(element)
+					append("i")
+							.attr("class", iconClass + " counter-icon")
+							.attr('style', 'top: ' + ((boxSize - iconSize) / 2) + 'px;' + 'font-size: ' + iconSize + 'px;' + 'margin-left: -'+ ((boxSize - iconSize) / 2) +'px;');
+
+
+			// Title
+			d3.select(element)
+					append('div')
+							.text(textTitle);
+
+			// Subtitle
+			d3.select(element)
+					append('div')
+							.attr('class', 'text-size-small text-muted')
+							.text(textAverage);
+
+
+
+			// Animation
+			// ------------------------------
+
+			// Animate path
+			function updateProgress(progress) {
+					foreground.attr('d', arc.endAngle(twoPi * progress));
+					front.attr('d', arc.endAngle(twoPi * progress));
+					numberText.text(formatPercent(progress));
+			}
+
+			// Animate text
+			var progress = startPercent;
+			(function loops() {
+					updateProgress(progress);
+					if (count > 0) {
+							count--;
+							progress += step;
+							setTimeout(loops, 10);
+					}
+			})();
+	}
 });
